9f9d44baf64ac794862073ecc4f9d525334ed5cf
ehvenga <ehvenga@gmail.com>
2024-06-18 17:18:39 -0500

    hero

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <div>
+      <section className='text-2xl text-cyan-950'>
+        <h1 className='text-7xl'>
+          Hi, my
+          <br /> name is <span className='font-semibold'>Hari</span>
+        </h1>
+        <h2>
+          I'm a <span className='font-semibold'>Full-Stack Developer</span> from
+          Illinois
+        </h2>
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+// import { Switch } from '@/components/ui/switch';
+
+const Navbar = () => {
+  return (
+    <nav className='py-4 flex justify-between'>
+      <div>Navigation</div>
+      {/* <Switch /> */}
+    </nav>
+  );
+};
+
+export default Navbar;
